59a93a124652ad9c9e0bf5dabd32964334ffa65f
weiliang <weiliang.chong@day-care.cn>
2022-01-11 14:50:45 +0800

    fix path

--- processor/gen_kaldi_file.py
+++ processor/gen_kaldi_file.py
@@ -9,13 +9,15 @@ def gen(segments_dir):
     for root, dirs, files in os.walk(segments_dir):
         for f in files:
             if f.endswith('.wav'):
-                utt_id = f.split('.')[0]
+                p = f.rfind('.wav')
+                utt_id = f[:p]
                 wav_path = os.path.join(root, f)
                 wav_scp.append(f'{utt_id} {wav_path}')
                 text_path = os.path.join(root, utt_id+'.txt')
                 with open(text_path) as f:
                     content = f.read()
                 text.append(f'{utt_id} {content}')
+    segments_dir = segments_dir.strip('/')
     with open(f'{segments_dir}.wav.scp', 'w') as f:
         f.write('\n'.join(wav_scp))
     with open(f'{segments_dir}.text', 'w') as f:
