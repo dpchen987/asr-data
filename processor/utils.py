5d681a0c68001e57163c395246811b62b2c2b85f
weiliang <weiliang.chong@day-care.cn>
2022-03-02 11:00:26 +0800

    ffmpeg sometimes encodes bad opus which libsndfile can't read, so use opusenc in get_audio()

--- processor/utils.py
+++ processor/utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import re
 import difflib
 
@@ -41,10 +42,37 @@ def calc_similary(t1, t2):
     return differ.ratio()
 
 
+def get_audio(video_path, audio_path, channel=1, samplerate=48000, bitrate=32000):
+    args = [
+        f'-ac {channel}',
+        f'-ar {samplerate}',
+        f'-ab {bitrate}',
+        '-y',
+    ]
+    supported_format = {
+        'wav': '-map_metadata -1 -fflags +bitexact -acodec pcm_s16le',
+        'opus': ' -f wav - | opusenc - ',
+        'mp3': '-acodec libmp3lame',
+    } 
+    format = audio_path.split('.')[-1].lower()
+    if format not in supported_format:
+        raise ValueError(f'not supported audio format: {format}')
+    args.append(supported_format[format])
+    args = ' '.join(args)
+    cmd = f'ffmpeg -i {video_path} {args} {audio_path}'
+    print(cmd)
+    os.system(cmd)
+
+
+
 if __name__ == '__main__':
     from sys import argv
     opt = argv[1]
-    if opt == 'nor':
+    if opt == 'get':
+        video_path = argv[2]
+        audio_path = argv[3]
+        get_audio(video_path, audio_path)
+    elif opt == 'nor':
         a = 'a:，。,+\\、'
         n = text_normalize(a)
         print(a)
