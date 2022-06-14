277741637b1212c922889f2e599421c9ec32b5a5
weiliang <weiliang.chong@day-care.cn>
2022-06-15 01:13:28 +0800

    change skip_frame, and rules to find_end_start() to improve speed of processing

--- extractor/video_subtitle_extractor.py
+++ extractor/video_subtitle_extractor.py
@@ -150,6 +150,7 @@ def find_end_start(subtitles, buffer, sub_statistic):
     last_end = 0
     current_start = 0
     current_text = ''
+    buffer_texts = []
     while 1:
         #print(f'{pos=}', len(buffer))
         ts, frame = buffer[pos]
@@ -171,6 +172,7 @@ def find_end_start(subtitles, buffer, sub_statistic):
                     texts = OCR.ocr(sub_img, det=False, rec=True, cls=False)
                     texts = [t[0] for t in texts if t[1] > 0.6]
                     text = text_normalize(''.join(texts))
+                    buffer_texts.append((ts, text))
         if text != last_text:
             # go left
             right = pos
@@ -184,12 +186,10 @@ def find_end_start(subtitles, buffer, sub_statistic):
             last_end = ts
         if pos == left or pos == right:
             break
-    if last_end:
-        print(f'\t{last_end=}, {last_text=}')
-        subtitles.append((last_end, last_text))
-    if current_start:
-        print(f'\t{current_start=}, {current_text=}')
-        subtitles.append((current_start, current_text))
+    if buffer_texts:
+        buffer_texts.sort(key=lambda a:a[0])
+        print(buffer_texts)
+        subtitles.extend(buffer_texts)
 
 
 def find_subtitle_area(areas):
@@ -212,11 +212,9 @@ def find_subtitle_area(areas):
                 grouped.add(j)
         groups.append(group)
     # 取x_end变化最多的group
-    print('grous:', len(groups))
     group_count = {}
     for i, group in enumerate(groups):
         count = 0
-        print('\tgrous:', len(group), i)
         for j in range(1, len(group)):
             if abs(group[j][3] - group[j-1][3]) >= 1 * group[j][2]:
                 # x end 差值大于1个字高
@@ -224,7 +222,6 @@ def find_subtitle_area(areas):
         group_count[i] = count
     zz = sorted(group_count.items(), key=lambda a: a[1], reverse=True)
     idx = zz[0][0]
-    print('xxx', group_count, idx)
     best = groups[idx]
     return best
 
@@ -266,7 +263,7 @@ def extract_subtitle_raw(video_path):
     fps = cap.get(cv2.CAP_PROP_FPS)
     # 跳过n帧以加速提取，find_end()从跳过的帧中找到变化位置，
     # 但其假设是n帧中字幕只变化了一次，故n不能过大，否则会漏掉时间很短的字幕
-    skip_frame = int(fps)
+    skip_frame = int(fps * 1.2)
     # 第一次遍历video，识别main_frame(非skipped)，然后统计出视频位置：left/middle/right, 高度、高度位置
     i = 0
     while (cap.isOpened()):
@@ -284,12 +281,14 @@ def extract_subtitle_raw(video_path):
     cap.release()
     sub_statistic = subtitle_statistic(main_frame_subtitles)
     print('=== sub_statistic:', sub_statistic)
+    print(f'{len(main_frame_subtitles)=}')
     # 第二次遍历，识别skip frame，找到更精确的字幕开始结束时间
     buffer = []  # 缓存被skip掉的帧, [(timestamp, frame), ]
     subtitles = []  # [(timestamp, text), ]
     cap = cv2.VideoCapture(video_path)
     buffer = []
     i = 0
+    last_frame_has_text = False
     while (cap.isOpened()):
         frame_exists, frame = cap.read()
         if not frame_exists:
@@ -299,7 +298,7 @@ def extract_subtitle_raw(video_path):
         if i % skip_frame != 0:
             buffer.append((current, frame))
             continue
-        print(f'frame: {i}')
+        # print(f'frame: {i}')
         ts, subs = main_frame_subtitles[i]
         if subs is None:
             text = ''
@@ -318,19 +317,22 @@ def extract_subtitle_raw(video_path):
                     texts = OCR.ocr(sub_img, det=False, rec=True, cls=False)
                     texts = [t[0] for t in texts if t[1] > 0.6]
                     text = text_normalize(''.join(texts))
-        if not text and i == 1:
-            continue
+        # if not text and i == 1:
+        #     continue
         if not text:
-            find_end_start(subtitles, buffer, sub_statistic)
+            if last_frame_has_text:
+                find_end_start(subtitles, buffer, sub_statistic)
             buffer.clear()
+            last_frame_has_text = False
             continue
         if not subtitles:
             find_end_start(subtitles, buffer, sub_statistic)
         elif calc_similary(text, subtitles[-1][1]) < SIM_THRESH:
             find_end_start(subtitles, buffer, sub_statistic)
-        print(current, text)
+        # print(f'frame:{i=}, {current=}, {text=}')
         subtitles.append((current, text))
         buffer.clear()
+        last_frame_has_text = True
     cap.release()
     return subtitles
 
@@ -350,20 +352,23 @@ if __name__ == '__main__':
     if opt == 'det':
         fn = argv[2]
         img = cv2.imread(fn)
-        b = time.time()
         init()
+        bt = time.time()
         box = detect_subtitle(img)
+        print('detect time:', time.time() - bt)
+        bt = time.time()
         for b in box:
             print(b)
             im = crop_origin_sub(b['sub_box'], CROP_HEIGHT_RATIO, img)
             texts = OCR.ocr(im, det=False, rec=True, cls=False)
             texts = [t[0] for t in texts]
             print(''.join(texts), '\n\n')
+        print('rec time:', time.time() - bt)
     elif opt == 'ext':
         vp = argv[2]
         save_dir = '.'
         subs = extract_subtitle(vp, save_dir)
         import utils
-        utils.save_list(subs, 'z-ocr.txt')
+        utils.save_list(subs, 'z-ocr-.txt')
     else:
         print('nothing')
