b11d42b0e325d29d122499a20c24cf902dc4de50
weiliang <weiliang.chong@day-care.cn>
2022-11-15 09:34:00 +0800

    add check_oov()

--- wenet_shard/hanzi_asr_to_units.py
+++ wenet_shard/hanzi_asr_to_units.py
@@ -4,8 +4,10 @@
 
 import re
 
+P_HANZI = re.compile(r'[\u4E00-\u9FA5]+')
 
-def main():
+
+def gen_units():
     name = './hanzi_asr.txt'
     hanzi = []
     with open(name) as f:
@@ -23,4 +25,29 @@ def main():
     print('done, tokens:', len(units))
 
 
-main()
+def check_oov(fn):
+    units = set()
+    with open('./hanzi_asr.txt') as f:
+        for line in f:
+            zi = line.strip()
+            if not zi:
+                continue
+            units.add(zi)
+    oov_count = 0
+    with open(fn) as f:
+        for line in f:
+            hanzi = ''.join(P_HANZI.findall(line))
+            for zi in hanzi:
+                if zi not in units:
+                    print('oov:', zi, line)
+                    oov_count += 1
+    print(f'{oov_count = }')
+
+
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) > 1:
+        fn = argv[1]
+        check_oov(fn)
+    else:
+        gen_units()
